b1d8264e43e8eefd618fbeb1c697d1eb93725492
santohack <santohack238@gmail.com>
2023-10-11 11:56:09 +0530

    Update frontend code

--- frontend/src/components/pages/navbar/DesktopNavBar.js
+++ frontend/src/components/pages/navbar/DesktopNavBar.js
@@ -3,34 +3,45 @@ import { NavContainer, Search, SearchIconWrapper, StyledInputBase } from '../../
 import { AppBar, Toolbar, Typography } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 import NavIcon from './NavIcon';
-const DesktopNavBar = ({match}) => {
+/**
+ * Renders the desktop navigation bar component.
+ *
+ * @param {object} match - The match object containing the URL parameters.
+ * @returns {ReactNode} The rendered navigation bar component.
+ */
+const DesktopNavBar = ({ match }) => {
   return (
     <>
+      {/* Container for the navigation bar */}
       <NavContainer>
+        {/* App bar component */}
         <AppBar position="static">
+          {/* Toolbar component */}
           <Toolbar>
-
+            {/* Typography component for the logo */}
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               ShopCart
             </Typography>
+            {/* Search component */}
             <Search>
+              {/* Wrapper for the search icon */}
               <SearchIconWrapper>
+                {/* Search icon */}
                 <SearchIcon />
               </SearchIconWrapper>
+              {/* Input base component for search input */}
               <StyledInputBase
                 placeholder="Search…"
                 inputProps={{ 'aria-label': 'search' }}
               />
             </Search>
+            {/* Navigation icon component */}
             <NavIcon match={match} />
-
-           
           </Toolbar>
         </AppBar>
       </NavContainer>
-
     </>
-  )
-}
+  );
+};
 
 export default DesktopNavBar
