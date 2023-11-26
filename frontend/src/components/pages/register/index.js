a2823c9dbdd674600121b89243b808a10b65d456
santohack <santohack238@gmail.com>
2023-11-26 19:10:29 +0530

    OK

--- frontend/src/components/pages/register/index.js
+++ frontend/src/components/pages/register/index.js
@@ -4,33 +4,33 @@ import { LoginContainer } from '../../styles/login';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useRegisterMutation } from '../../../slices/userApiSlice'; 
+import { useRegisterMutation } from '../../../slices/userApiSlice';
 import { setCrediential } from '../../../slices/authslice';
 import { toast } from 'react-toastify';
 const RegsiterScreen = () => {
-const dispatch = useDispatch();
-const navigate = useNavigate();
-const { search } = useLocation();
-const sp = new URLSearchParams(search);
-const redirect = sp.get('redirect') || '/';
-const [register, { isLoading }] = useRegisterMutation();
-const  { userInfo } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { search } = useLocation();
+    const sp = new URLSearchParams(search);
+    const redirect = sp.get('redirect') || '/';
+    const [register, { isLoading }] = useRegisterMutation();
+    const { userInfo } = useSelector((state) => state.auth);
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
- useEffect(() => {
-    if (userInfo) {
-        navigate(redirect);
-    }
- }, [navigate, redirect, userInfo]);
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo]);
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(password !== confirmPassword) {
-           toast.error("Passwords do not match");
+        if (password !== confirmPassword) {
+            toast.error("Passwords do not match");
             return;
         }
-        else{
+        else {
             try {
                 const res = register({ email, password, name }).unwrap();
                 dispatch(setCrediential({ ...res }));
@@ -41,8 +41,8 @@ const  { userInfo } = useSelector((state) => state.auth);
                 console.log("error", error);
             }
         }
-       
-       
+
+
     }
     return (
         <>
@@ -79,18 +79,18 @@ const  { userInfo } = useSelector((state) => state.auth);
                             onChange={(e) => setPassword(e.target.value)}
                             variant="outlined"
                         />
-                         <TextField
-                        id="password-input"
-                        label="Confirm Password"
-                        type="password"  // Added for password masking
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        variant="outlined"
-                    />
+                        <TextField
+                            id="password-input"
+                            label="Confirm Password"
+                            type="password"  // Added for password masking
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            variant="outlined"
+                        />
 
                         <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginRight: '50px', justifyContent: 'space-between' }}>
                             <Button type="submit" sx={{ width: '20ch' }} variant="contained">Register</Button>
-                            <Typography variant="body1" sx={{ marginLeft: '10px' }}>Already have an account? <Link to={redirect?  `/login?redirect=${redirect}` : `/login`}>LogIn</Link></Typography>
+                            <Typography variant="body1" sx={{ marginLeft: '10px' }}>Already have an account? <Link to={redirect ? `/login?redirect=${redirect}` : `/login`}>LogIn</Link></Typography>
                         </div>
                     </FormControl>
                 </Box>
