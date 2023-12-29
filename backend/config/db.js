c192d5790249950b7fc393252bc043af2b1b0c6a
santohack <santohack238@gmail.com>
2023-12-30 03:56:03 +0530

    reame

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
+/**
+ * Connects to the MongoDB database using the MONGO_URL environment variable.
+ *
+ * @return {Promise<void>} - This function does not return anything.
+ */
 const connectDB = async ()=>{
    try {
      const connect = await mongoose.connect(process.env.MONGO_URL)
